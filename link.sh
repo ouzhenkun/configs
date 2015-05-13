6bf3e2846e58b3de95af209b25a4cc3f13f4cd8b
zhenkunou <ryan.ouzhenkun@gmail.com>
2015-05-13 15:33:04 +0800

    update link.sh

--- link.sh
+++ link.sh
@@ -2,13 +2,8 @@
 
 CONFIGS=$HOME/configs
 SHADOWSOCKSX=$HOME/.ShadowsocksX
-ATOM=$HOME/.atom
 
 ln -sf $CONFIGS/.gitconfig     $HOME/.gitconfig
 ln -sf $CONFIGS/.gitignore     $HOME/.gitignore
 ln -sf $CONFIGS/.bash_profile  $HOME/.bash_profile
 ln -sf $CONFIGS/gfwlist.js     $SHADOWSOCKSX/gfwlist.js
-
-ln -sf $CONFIGS/.atom/keymap.cson $ATOM/keymap.cson
-ln -sf $CONFIGS/.atom/config.cson $ATOM/config.cson
-ln -sf $CONFIGS/.atom/styles.less $ATOM/styles.less
