d1f6f21db05680a46c0592e68b5354315607dd10
zhenkunou <ryan.ouzhenkun@gmail.com>
2015-07-23 14:43:31 +0800

    Add tmux conf

--- link.sh
+++ link.sh
@@ -8,3 +8,4 @@ ln -sf $CONFIGS/.gitignore     $HOME/.gitignore
 ln -sf $CONFIGS/.bash_profile  $HOME/.bash_profile
 ln -sf $CONFIGS/gfwlist.js     $SHADOWSOCKSX/gfwlist.js
 ln -sf $CONFIGS/ryan.zsh-theme $HOME/.oh-my-zsh/themes/ryan.zsh-theme
+ln -sf $CONFIGS/.tmux.conf     $HOME/.tmux.conf
