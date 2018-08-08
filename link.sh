9b45912b7e16792da376a825bb3cf11bf42fe4a3
ouzhenkun <ryan.ouzhenkun@gmail.com>
2018-08-08 12:40:06 +0800

    Add zshrc config

--- link.sh
+++ link.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 CONFIGS=$HOME/configs
-SHADOWSOCKSX=$HOME/.ShadowsocksX
 
 ln -sf $CONFIGS/.gitconfig     $HOME/.gitconfig
 ln -sf $CONFIGS/.gitignore     $HOME/.gitignore
-ln -sf $CONFIGS/.bash_profile  $HOME/.bash_profile
-ln -sf $CONFIGS/gfwlist.js     $SHADOWSOCKSX/gfwlist.js
 ln -sf $CONFIGS/ryan.zsh-theme $HOME/.oh-my-zsh/themes/ryan.zsh-theme
+ln -sf $CONFIGS/.zshrc         $HOME/.zshrc
 ln -sf $CONFIGS/.tmux.conf     $HOME/.tmux.conf
 ln -sf $CONFIGS/.eslintrc      $HOME/.eslintrc
